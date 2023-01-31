fe5bdc3349029e88dbb2e7ec5bec1eb24ef122dd
Polina Fedulova <FEDPolina_A@bk.ru>
2023-01-31 14:53:26 +0300

    -

--- PractGit/Form1.Designer.cs
+++ PractGit/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.textBox_add = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox_sub = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -75,11 +77,29 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "+";
             // 
+            // textBox_sub
+            // 
+            this.textBox_sub.Location = new System.Drawing.Point(145, 105);
+            this.textBox_sub.Name = "textBox_sub";
+            this.textBox_sub.Size = new System.Drawing.Size(75, 20);
+            this.textBox_sub.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "-";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(501, 267);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_sub);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox_add);
@@ -99,6 +119,8 @@
         private System.Windows.Forms.TextBox textBox_add;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_sub;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- PractGit/Form1.cs
+++ PractGit/Form1.cs
@@ -22,6 +22,7 @@ namespace PractGit
         {
 
             textBox_add.Text = (int.Parse(textBox1.Text) + int.Parse(textBox2.Text)).ToString();
+            textBox_sub.Text = (int.Parse(textBox1.Text) - int.Parse(textBox2.Text)).ToString();
 
         }
     }
